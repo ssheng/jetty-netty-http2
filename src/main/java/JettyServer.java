ade026c967a0cb9b95afb9cb14f135a054646d88
Sean Sheng <s3sheng@gmail.com>
2016-03-04 14:26:59 -0800

    Setup Jetty server and Netty client

---
+++ src/main/java/JettyServer.java
@@ -0,0 +1,81 @@
+import org.eclipse.jetty.http2.server.HTTP2CServerConnectionFactory;
+import org.eclipse.jetty.server.HttpConfiguration;
+import org.eclipse.jetty.server.HttpConnectionFactory;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+
+public class JettyServer
+{
+  private static final int HTTP_PORT = 8080;
+  private static final int HTTPS_PORT = 8443;
+
+  private final Server _server;
+
+  public JettyServer()
+  {
+    _server = new Server();
+
+    // HTTP Configuration
+    HttpConfiguration configuration = new HttpConfiguration();
+    configuration.setSecureScheme("https");
+    configuration.setSecurePort(HTTPS_PORT);
+    configuration.setSendXPoweredBy(true);
+    configuration.setSendServerVersion(true);
+
+    // HTTP Connector
+    ServerConnector http = new ServerConnector(
+        _server,
+        new HttpConnectionFactory(configuration),
+        new HTTP2CServerConnectionFactory(configuration));
+    http.setPort(HTTP_PORT);
+    _server.addConnector(http);
+
+    /*
+    // SSL Context Factory for HTTPS and HTTP/2
+    String jetty_distro = System.getProperty("jetty.distro","../../jetty-distribution/target/distribution");
+    SslContextFactory sslContextFactory = new SslContextFactory();
+    sslContextFactory.setKeyStorePath(jetty_distro + "/demo-base/etc/keystore");
+    sslContextFactory.setKeyStorePassword("OBF:1vny1zlo1x8e1vnw1vn61x8g1zlu1vn4");
+    sslContextFactory.setKeyManagerPassword("OBF:1u2u1wml1z7s1z7a1wnl1u2g");
+    sslContextFactory.setCipherComparator(HTTP2Cipher.COMPARATOR);
+
+    // HTTPS Configuration
+    HttpConfiguration https_config = new HttpConfiguration(configuration);
+    https_config.addCustomizer(new SecureRequestCustomizer());
+
+    // HTTP/2 Connection Factory
+    HTTP2ServerConnectionFactory h2 = new HTTP2ServerConnectionFactory(https_config);
+
+    NegotiatingServerConnectionFactory.checkProtocolNegotiationAvailable();
+    ALPNServerConnectionFactory alpn = new ALPNServerConnectionFactory();
+    alpn.setDefaultProtocol(http.getDefaultProtocol());
+
+    // SSL Connection Factory
+    SslConnectionFactory ssl = new SslConnectionFactory(sslContextFactory,alpn.getProtocol());
+
+    // HTTP/2 Connector
+    ServerConnector http2Connector =
+        new ServerConnector(_server,ssl,alpn,h2,new HttpConnectionFactory(https_config));
+    http2Connector.setPort(HTTPS_PORT);
+    _server.addConnector(http2Connector);
+    */
+
+    ServletContextHandler handler = new ServletContextHandler(_server, "");
+    handler.addServlet(new ServletHolder(new AckServlet()), "/*");
+  }
+
+  public Server instance()
+  {
+    return _server;
+  }
+
+  public static void main(String[] args) throws Exception
+  {
+    JettyServer server = new JettyServer();
+    server.instance().start();
+    server.instance().join();
+  }
+}
---
+++ src/main/java/NettyHttp2ClientUpgradeCodec.java
@@ -0,0 +1,35 @@
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http2.Http2ClientUpgradeCodec;
+import io.netty.handler.codec.http2.Http2ConnectionHandler;
+import io.netty.util.AsciiString;
+import java.util.Collection;
+
+import static io.netty.handler.codec.http2.Http2CodecUtil.HTTP_UPGRADE_SETTINGS_HEADER;
+
+
+public class NettyHttp2ClientUpgradeCodec extends Http2ClientUpgradeCodec
+{
+  static final boolean SSL = System.getProperty("ssl") != null;
+  static final int PORT = Integer.parseInt(System.getProperty("port", SSL? "8443" : "8080"));
+  static final String HOST = System.getProperty("host", "127.0.0.1");
+  static AsciiString hostName = new AsciiString(HOST + ':' + PORT);
+
+  public NettyHttp2ClientUpgradeCodec(Http2ConnectionHandler connectionHandler)
+  {
+    super(connectionHandler);
+  }
+
+  public NettyHttp2ClientUpgradeCodec(String handlerName, Http2ConnectionHandler connectionHandler)
+  {
+    super(handlerName, connectionHandler);
+  }
+
+  @Override
+  public Collection<CharSequence> setUpgradeHeaders(ChannelHandlerContext ctx,
+      HttpRequest upgradeRequest) {
+    upgradeRequest.headers().set(HttpHeaderNames.HOST, hostName);
+    return super.setUpgradeHeaders(ctx, upgradeRequest);
+  }
+}
